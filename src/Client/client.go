7c6b5af7c7c7d83b735f91abe642d9f2ae78a3c1
johnpoint <me@lvcshu.com>
2019-10-17 08:12:56 +0800

    update

--- src/Client/client.go
+++ src/Client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -27,19 +28,28 @@ type Data struct {
 	Base         DataBase
 	Sites        []DataSite
 	Certificates []DataCertificate
+	Services     []DataService
 }
 
 type DataBase struct {
-	ServerIpv4 string
-	ServerIpv6 string
-	HostName   string
-	Token      string
+	ServerIpv4  string
+	ServerIpv6  string
+	HostName    string
+	Token       string
+	PollAddress string
 }
 
 type DataSite struct {
 	Domain string
 	Enable bool
 	CerID  int64
+}
+
+type DataService struct {
+	Name    string
+	Enable  string
+	Disable string
+	Status  string
 }
 
 type DataCertificate struct {
@@ -85,6 +95,11 @@ type InterfaceInfo struct {
 	ByteRecv uint64
 }
 
+type Webreq struct {
+	Code int64  `json:Code`
+	Info string `json:Info`
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("请输入参数")
@@ -93,20 +108,22 @@ func main() {
 	if os.Args[1] == "install" {
 		//install `hostname` `curl ip.sb`
 		setup(os.Args)
+		statuspoll()
 		return
 	}
 	if os.Args[1] == "poll" {
 		statuspoll()
 		return
 	}
-	if os.Args[1] == "data" {
-		fmt.Println(getData())
+	if os.Args[1] == "debug" {
+		delSite("lvcshu.com")
 		return
 	}
 	fmt.Println("未知的参数")
 }
 
 func setup(args []string) {
+	data := Data{}
 	url := args[2] + "/server/setup"
 	method := "POST"
 
@@ -127,16 +144,149 @@ func setup(args []string) {
 	res, err := client.Do(req)
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
+	webreq := Webreq{}
+	json.Unmarshal([]byte(body), &webreq)
+	if webreq.Code != 200 {
+		fmt.Println(webreq.Info)
+		return
+	}
+	base := DataBase{ServerIpv4: args[4], HostName: args[3], Token: webreq.Info, PollAddress: args[2]}
+	data.Base = base
+	file, _ := os.Create("data.json")
+	defer file.Close()
+	databy, err := json.Marshal(data)
+	_, err1 := io.WriteString(file, string(databy))
+	if err1 != nil {
+		panic(err1)
+	}
+	fmt.Println("OK!")
+}
 
-	fmt.Println(string(body))
+func addService(name string, enable string, disable string) bool {
+	data := getData()
+	for index := 0; index < len(data.Services); index++ {
+		if data.Services[index].Name == name {
+			fmt.Println("Service already exists")
+			return false
+		}
+	}
+	data.Services = append(data.Services, DataService{Name: name, Enable: enable, Disable: disable, Status: "stop"})
+	file, _ := os.Create("data.json")
+	defer file.Close()
+	databy, _ := json.Marshal(data)
+	_, err := io.WriteString(file, string(databy))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("OK!")
+	return true
+}
+
+func delService(name string) bool {
+	data := getData()
+	for index := 0; index < len(data.Services); index++ {
+		if data.Services[index].Name == name {
+			data.Services = append(data.Services[:index], data.Services[index+1:]...)
+			file, _ := os.Create("data.json")
+			defer file.Close()
+			databy, _ := json.Marshal(data)
+			_, err := io.WriteString(file, string(databy))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("OK!")
+			return true
+		}
+	}
+	fmt.Println("Service not exists")
+	return false
+}
+
+func addSite(domain string, enable bool, cerid int64) bool {
+	data := getData()
+	for index := 0; index < len(data.Sites); index++ {
+		if data.Sites[index].Domain == domain {
+			fmt.Println("Site already exists")
+			return false
+		}
+	}
+	data.Sites = append(data.Sites, DataSite{Domain: domain, Enable: enable, CerID: cerid})
+	file, _ := os.Create("data.json")
+	defer file.Close()
+	databy, _ := json.Marshal(data)
+	_, err := io.WriteString(file, string(databy))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("OK!")
+	return true
+}
+
+func delSite(domain string) bool {
+	data := getData()
+	for index := 0; index < len(data.Sites); index++ {
+		if data.Sites[index].Domain == domain {
+			data.Sites = append(data.Sites[:index], data.Sites[index+1:]...)
+			file, _ := os.Create("data.json")
+			defer file.Close()
+			databy, _ := json.Marshal(data)
+			_, err := io.WriteString(file, string(databy))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("OK!")
+			return true
+		}
+	}
+	fmt.Println("Site not exists")
+	return false
+}
+
+func addCer(id int64, domain string, fullchain string, key string) bool {
+	data := getData()
+	for index := 0; index < len(data.Certificates); index++ {
+		if data.Certificates[index].ID == id {
+			fmt.Println("Certificate already exists")
+			return false
+		}
+	}
+	data.Certificates = append(data.Certificates, DataCertificate{ID: id, Domain: domain, FullChain: fullchain, Key: key})
+	file, _ := os.Create("data.json")
+	defer file.Close()
+	databy, _ := json.Marshal(data)
+	_, err := io.WriteString(file, string(databy))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("OK!")
+	return true
+}
+
+func delCer(id int64) bool {
+	data := getData()
+	for index := 0; index < len(data.Certificates); index++ {
+		if data.Certificates[index].ID == id {
+			data.Certificates = append(data.Certificates[:index], data.Certificates[index+1:]...)
+			file, _ := os.Create("data.json")
+			defer file.Close()
+			databy, _ := json.Marshal(data)
+			_, err := io.WriteString(file, string(databy))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("OK!")
+			return true
+		}
+	}
+	fmt.Println("Certificate not exists")
+	return false
 }
 
 func statuspoll() {
 	data := getData()
-	conf := loadConfig()
-	fmt.Println("[ Poll start ] To " + conf.PollAddress)
+	fmt.Println("[ Poll start ] To " + data.Base.PollAddress)
 	for true {
-		url := conf.PollAddress + "/server/update/" + conf.ServerToken
+		url := data.Base.PollAddress + "/server/update/" + data.Base.Token
 		method := "POST"
 
 		payload := strings.NewReader("ipv4=" + data.Base.ServerIpv4 + "&token=" + data.Base.Token + "&status=" + infoMiniJSON())
--- src/Client/client_test.go
+++ src/Client/client_test.go
@@ -9,3 +9,19 @@ func TestLoadConfig(t *testing.T) {
 func TestGetData(t *testing.T) {
 	t.Log(getData())
 }
+
+func TestAddCer(t *testing.T) {
+	t.Log(addCer(12, "lvcshu.info", "fullchain", "key"))
+}
+
+func TestAddService(t *testing.T) {
+	if addService(":", "enable string", "disablestring") == false {
+		t.Log("success")
+	} else {
+		t.Error(addService(":", "enable string", "disablestring"))
+	}
+}
+
+func TestDelCer(t *testing.T) {
+	t.Log(delCer(12))
+}
