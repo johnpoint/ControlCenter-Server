5e3ecf840c345affa75555c166e60632542c2eba
johnpoint <me@lvcshu.com>
2020-09-26 12:57:27 +0800

    update :: 服务端下发文件配置增加id字段

--- src/apis/server.go
+++ src/apis/server.go
@@ -72,7 +72,7 @@ func GetServerUpdate(c echo.Context) error {
 			for i := 0; i < len(getConfID); i++ {
 				if getConfID[i].ItemID != 0 {
 					conf := database.GetConfiguration(model.Configuration{ID: getConfID[i].ItemID})[0]
-					ConfFile = append(ConfFile, model.File{Name: conf.Name, Value: conf.Value, Path: conf.Path})
+					ConfFile = append(ConfFile, model.File{ID: conf.ID, Name: conf.Name, Value: conf.Value, Path: conf.Path})
 				}
 			}
 			data.ConfFile = ConfFile
