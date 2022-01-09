e28f58ae41c6f8eaa9f48f08399c58da5749f05e
johnpoint <me@lvcshu.com>
2022-01-09 15:45:57 +0800

    feat: 分离基础服务启动命令

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -11,11 +11,27 @@ export INFLUXDB_PASSWORD="password"
 export REDIS_PASSWORD="123456"
 
 if [[ $1 = "up" ]]; then
-  docker-compose -f base_service.yaml up -d
-  docker-compose -f server.yaml up -d
+  if [[ $2 = "" ]]; then
+    docker-compose -f base_service.yaml up -d
+    docker-compose -f server.yaml up -d
+  fi
+  if [[ $2 = "base" ]]; then
+    docker-compose -f base_service.yaml up -d
+  fi
+  if [[ $2 = "dev" ]]; then
+    docker-compose -f server.yaml up -d
+  fi
 fi
 
 if [[ $1 = "down" ]]; then
-  docker-compose -f base_service.yaml down
-  docker-compose -f server.yaml down
+  if [[ $2 = "" ]]; then
+    docker-compose -f base_service.yaml down
+    docker-compose -f server.yaml down
+  fi
+  if [[ $2 = "base" ]]; then
+    docker-compose -f base_service.yaml down
+  fi
+  if [[ $2 = "dev" ]]; then
+    docker-compose -f server.yaml down
+  fi
 fi
