85037ae5e71e1e4b461618727cbb6901fb0961b6
johnpoint <me@lvcshu.com>
2022-01-16 00:28:24 +0800

    error: 添加错误类型

--- infra/error.go
+++ infra/error.go
@@ -8,10 +8,11 @@ var (
 	ReqSameUsernameError = &errorHelper.Err{Code: 40101, Message: "有相同用户名"}
 
 	// 认证异常 403xx
-	ErrNeedLogin       = &errorHelper.Err{Code: 40301, Message: "此区域需要登录访问"}
-	ErrNeedVerifyInfo  = &errorHelper.Err{Code: 40302, Message: "请求需要身份认证信息"}
-	ErrAuthInfoInvalid = &errorHelper.Err{Code: 40303, Message: "身份认证失败"}
-	ErrAuthServise     = &errorHelper.Err{Code: 40304, Message: "认证服务异常"}
+	ErrNeedLogin            = &errorHelper.Err{Code: 40301, Message: "此区域需要登录访问"}
+	ErrNeedVerifyInfo       = &errorHelper.Err{Code: 40302, Message: "请求需要身份认证信息"}
+	ErrAuthInfoInvalid      = &errorHelper.Err{Code: 40303, Message: "身份认证失败"}
+	ErrAuthService          = &errorHelper.Err{Code: 40304, Message: "认证服务异常"}
+	ErrAssetsAuthorityError = &errorHelper.Err{Code: 40305, Message: "资产获取异常"}
 
 	// 数据库异常 501xx
 	DataBaseError = &errorHelper.Err{Code: 50100, Message: "数据库异常"}
