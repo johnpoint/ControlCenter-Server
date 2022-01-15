5b5760fd441cf3d6d14b0a453f225276f1094df8
johnpoint <me@lvcshu.com>
2022-01-15 15:46:13 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
-while ! nc -z base_service_mq 5672; do
-  echo "wait for control_center_mq"
+while ! nc -z base_service_rabbitmq 5672; do
+  echo "wait for base_service_rabbitmq"
   sleep 1
 done
 while ! nc -z base_service_mongodb 27017; do
-  echo "wait for mongodb"
+  echo "wait for base_service_mongodb"
   sleep 1
 done
 while ! nc -z base_service_influxdb 8086; do
-  echo "wait for redis"
+  echo "wait for base_service_influxdb"
   sleep 1
 done
 while ! nc -z base_service_redis 6379; do
-  echo "wait for redis"
+  echo "wait for base_service_redis"
   sleep 1
 done
 /usr/src/ControlCenter $1 --config /usr/src/config_dev.json
