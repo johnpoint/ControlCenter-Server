5a7c060ef40ae9d93ebf4e73e0a1faa51b434001
johnpoint <me@lvcshu.com>
2022-02-07 16:09:02 +0800

    fix: 更新 日志 输出结构

--- depend/logger.go
+++ depend/logger.go
@@ -5,6 +5,7 @@ import (
 	"ControlCenter/pkg/bootstrap"
 	"ControlCenter/pkg/log"
 	"context"
+	"go.uber.org/zap/zapcore"
 )
 
 type Logger struct{}
@@ -14,10 +15,18 @@ var _ bootstrap.Component = (*Logger)(nil)
 func (d *Logger) Init(ctx context.Context) error {
 	var options = make([]log.Option, 0)
 	if config.Config.Environment == "local" {
-		options = append(options, log.WithConsoleEncoding())
+		options = append(options,
+			log.WithConsoleEncoding(),
+			log.WrapLevelEncoder(zapcore.CapitalColorLevelEncoder),
+			log.WrapTimeEncoder(zapcore.ISO8601TimeEncoder),
+		)
 	} else {
-		options = append(options, log.WithJSONEncoding())
+		options = append(options,
+			log.WithJSONEncoding(),
+			log.WrapLevelEncoder(zapcore.CapitalLevelEncoder),
+		)
 	}
+
 	log.OverrideLoggerWithOption(map[string]interface{}{
 		"service-name": config.Config.ServiceName,
 	}, options...)
