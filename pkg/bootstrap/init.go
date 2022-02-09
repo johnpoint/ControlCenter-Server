eddba4abd4b13e62423ca9198b08a3e3971b567c
johnpoint <me@lvcshu.com>
2022-02-09 12:06:53 +0800

    [pkg] feat: 简化初始化流程 & 不加载全局组件方法

--- pkg/bootstrap/init.go
+++ pkg/bootstrap/init.go
@@ -15,14 +15,18 @@ func AddGlobalComponent(components ...Component) {
 	globalComponent = append(globalComponent, components...)
 }
 
+func NewBoot(ctx context.Context, components ...Component) *Helper {
+	return &Helper{
+		components: components,
+	}
+}
+
 type Helper struct {
 	*log.Logger
 	components []Component
 }
 
-func (i *Helper) Init(ctx context.Context) error {
-	fmt.Println("[Bootstrap] Start")
-	rand.Seed(time.Now().UnixNano())
+func (i *Helper) loadGlobalComponent(ctx context.Context) error {
 	for j := range globalComponent {
 		fmt.Println(fmt.Sprintf("[Bootstrap] %s", reflect.TypeOf(globalComponent[j])))
 		err := globalComponent[j].Init(ctx)
@@ -30,12 +34,41 @@ func (i *Helper) Init(ctx context.Context) error {
 			return err
 		}
 	}
+	return nil
+}
+
+func (i *Helper) loadComponent(ctx context.Context) error {
 	for j := range i.components {
 		fmt.Println(fmt.Sprintf("[Bootstrap] %s", reflect.TypeOf(i.components[j])))
 		err := i.components[j].Init(ctx)
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func (i *Helper) InitWithoutGlobalComponent(ctx context.Context) error {
+	fmt.Println("[Bootstrap] Start")
+	rand.Seed(time.Now().UnixNano())
+	err := i.loadComponent(ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Println("[Bootstrap] Finish")
+	return nil
+}
+
+func (i *Helper) Init(ctx context.Context) error {
+	fmt.Println("[Bootstrap] Start")
+	rand.Seed(time.Now().UnixNano())
+	err := i.loadGlobalComponent(ctx)
+	if err != nil {
+		return err
+	}
+	err = i.loadComponent(ctx)
+	if err != nil {
+		return err
 	}
 	fmt.Println("[Bootstrap] Finish")
 	return nil
